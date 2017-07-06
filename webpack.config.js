93e59367d9cc023814604155337ad4e98ba3287d
Lars Henrik Nordli <larshenrik91@gmail.com>
2017-07-06 10:39:36 +0200

    Removed fonts + updated pipeline-config

--- webpack.config.js
+++ webpack.config.js
@@ -35,8 +35,8 @@ module.exports = {
          },
 
          //Vendor: Font Awesome Loaders
-         { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
-         { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
+         // { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
+         // { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
       ]
    },
    plugins: debug ? [
