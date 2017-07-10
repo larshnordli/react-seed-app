8671feb9af4d85e6aa869e649128e0d076c9fb34
Nordli <lars.henrik.nordli@accenture.com>
2017-07-10 10:20:08 +0200

    Temp webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -28,15 +28,8 @@ module.exports = {
           },
 
          // JS Loaders
-         {
-            test: /\.js?$/,
-            exclude: /(node_modules|bower_components)/,
-            loader: 'babel-loader',
-            options: {
-               presets: ['react', 'es2015', 'stage-0'],
-               plugins: ['react-html-attrs', 'transform-class-properties', 'transform-decorators-legacy']
-            }
-         },
+         { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+         { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ }
 
          //Vendor: Font Awesome Loaders
          { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&mimetype=application/font-woff" },
@@ -44,14 +37,15 @@ module.exports = {
       ]
    },
    plugins: [
-      new webpack.optimize.UglifyJsPlugin(
-         {
-            mangle:false,
-            sourcemap: false
-         }),
-      new ExtractTextPlugin('[name].css'),
+      new webpack.optimize.UglifyJsPlugin({
+        mangle:false,
+        sourcemap: false
+     }),
+     new ExtractTextPlugin('[name].css'),
      new HtmlWebpackPlugin({
-            template: __dirname + '/src/index.html'
-        })
+        template: __dirname + '/src/index.html',
+        filename: 'index.html',
+        inject: 'body'
+    })
    ],
 };
